0f003c42088e54a54adc3fd22941d50992fe622c
wrightm <wright1michael@gmail.com>
2014-02-18 17:23:27 +1100

    Added Exercise 2 from chapter 1

---
+++ ThinkStats/chapter1/exercise2.py
@@ -0,0 +1,15 @@
+'''
+Created on 18 Feb 2014
+
+@author: wrightm
+'''
+from chapter1.main.first import Summarize
+
+def main():
+    
+    data_dir = "../resources/"
+    Summarize(data_dir)
+
+
+if __name__ == '__main__':
+    main()
