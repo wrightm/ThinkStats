34d768b208d22dcb79e762561cf14d576d9985a8
wrightm <wright1michael@gmail.com>
2014-02-25 21:15:35 +1100

    Added Chapter 4 Exercise 1

---
+++ ThinkStats/chapter4/exercise1.py
@@ -0,0 +1,17 @@
+'''
+Created on 25 Feb 2014
+
+@author: wrightm
+'''
+from chapter4.main.continuous import MakeExpoCdf, MakeParetoCdf, MakeParetoCdf2
+
+def main():
+    
+    MakeExpoCdf()
+    
+    MakeParetoCdf()
+    
+    MakeParetoCdf2()
+    
+if __name__ == '__main__':
+    main()
