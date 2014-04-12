0231707456fcd687cdfa494c3b9224bb5d48a1b6
wrightm <wright1michael@gmail.com>
2014-04-12 15:24:35 +1000

    t-test added to exercise 2 chapter 5

--- ThinkStats/chapter5/main/poincare_baker.py
+++ ThinkStats/chapter5/main/poincare_baker.py
@@ -9,24 +9,36 @@ import random
 from chapter5.main import myplot
 from matplotlib import pyplot
 
+from scipy import stats
+
 def compare_poincare_baker():
 
     """
     bread measurements by poincare 365 fitted normal distribution with mean 950g and 50g standard deviation
     """ 
 
-    xs, ps  = RenderNormalCdf(950, 50, 1150, 365)
-    normal_cdf_distribution = Cdf(xs, ps, 'baker')
     poincare_sample = [ random.normalvariate(950, 50) for i in xrange(366) ]
-    
     poincare_sample_cdf = MakeCdfFromList(poincare_sample, 'poincare')
-
+    
+    baker_sample = []
+    for i in xrange(366):
+        baker_sample.append(max(random.normalvariate(950, 50) for i in xrange(4)))
+    baker_cdf = MakeCdfFromList(baker_sample)
+    
+        
+    print poincare_sample_cdf.Mean(),
+    print baker_cdf.Mean()
     myplot.Clf()
-    myplot.Cdfs([poincare_sample_cdf, normal_cdf_distribution])
+    myplot.Cdfs([poincare_sample_cdf, baker_cdf])
     pyplot.xlim(600, 1120)
     pyplot.legend(loc=0)
-    myplot.SaveFormat('../resources/plots/poincare_vs_baker')
-
+    myplot.SaveFormat('../resources/plots/poincare_vs_baker', 'png')
+    
+    # t-test
+    t_test = stats.ttest_rel(poincare_sample, baker_sample)
+    
+    print "t-test statistic is %s with a p-value %s" % t_test
+    
 def main():
     
     compare_poincare_baker()
